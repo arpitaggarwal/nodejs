3b9a636fcbecde11fd98bed499ecf7d9e8f405dd
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2016-11-20 19:26:55 +0400

    express - static and file handling

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+### Install Express
+
+```
 npm install express --save
 
 npm install body-parser --save
@@ -5,3 +8,11 @@ npm install body-parser --save
 npm install cookie-parser --save
 
 npm install multer --save
+
+```
+
+### Express Serve Static Files
+
+```
+http://localhost:8081/images/node.png
+```
--- express/server.js
+++ express/server.js
@@ -1,8 +1,57 @@
 var express = require('express');
 var app = express();
+var fs = require("fs");
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({dest:__dirname+'/file/uploads/'}).any());
+
 
 app.get('/', function (req, res) {
    res.send('Hello World');
+})
+
+app.get('/index', function (req, res) {
+   res.sendFile( __dirname + "/" + "index-get.html" );
+})
+
+app.get('/upload', function (req, res) {
+   res.sendFile( __dirname + "/" + "index-upload-file.html" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/file_upload', function (req, res) {
+   console.log(req.files.file.name);
+   console.log(req.files.file.path);
+   console.log(req.files.file.type);
+   var file = __dirname + "/" + req.files.file.name;
+
+   fs.readFile( req.files.file.path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files.file.name
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
 })
 
 var server = app.listen(8081, function () {
